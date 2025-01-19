b820db0d19191583d92aa01864ca730e098bf808
maximghgh <maximbas2005@gmail.com>
2025-01-19 11:55:22 +0400

    задание 5

--- задание 5/main.js
+++ задание 5/main.js
@@ -1,31 +1,92 @@
-// main.js
-
+let posts;
 // Объекты пользователей
 const user1 = {
   id: 1,
   name: "Иван",
   surname: "Иванов",
-  login: "ivanov123"
+  login: "ivanov123",
 };
 
 const user2 = {
   id: 2,
   name: "Петр",
   surname: "Петров",
-  login: "petrov456"
+  login: "petrov456",
 };
 
 // Объекты постов
-const posts = [
-  { id: 1, title: "пост1", description: "Описание первого поста", user_id: 1 },
-  { id: 2, title: "пост2", description: "Описание второго поста", user_id: 2 },
-  { id: 3, title: "пост3", description: "Описание третьего поста", user_id: 2 }
+beforeEach(() => {
+  posts = [
+    { id: 1, title: "пост1", description: "Описание первого поста", user_id: 1 },
+    { id: 2, title: "пост2", description: "Описание второго поста", user_id: 2 },
+    { id: 3, title: "пост3", description: "Описание третьего поста", user_id: 2 },
+  ];
+
+  jest.resetModules();
+  jest.doMock('./main', () => ({
+    ...jest.requireActual('./main'),
+    posts,
+  }));
+});
+
+// Объекты комментариев
+const comments = [
+  { id: 1, post_id: 1, text: "Отличный пост!" },
+  { id: 2, post_id: 2, text: "Спасибо за информацию." },
+  { id: 3, post_id: 3, text: "Очень полезно." },
 ];
 
-// Функция для получения постов пользователя по его id
+// Получить все посты пользователя
 function getPostsByUser(userId) {
-  return posts.filter(post => post.user_id === userId);
+  return posts.filter((post) => post.user_id === userId);
 }
 
-// Экспортируем функцию, чтобы её можно было тестировать
-module.exports = { getPostsByUser };
+// Удалить пост по ID
+function removePostById(postId) {
+  return posts.filter((post) => post.id !== postId);
+}
+
+// Получить все посты
+function getAllPosts() {
+  return posts;
+}
+
+// Удалить все посты пользователя по user_id
+function deleteAllPostsByUserId(userId) {
+  const initialLength = posts.length;
+  for (let i = posts.length - 1; i >= 0; i--) {
+    if (posts[i].user_id === userId) {
+      posts.splice(i, 1);
+    }
+  }
+  console.log(
+    `Удалено ${initialLength - posts.length} постов пользователя с ID ${userId}.`
+  );
+}
+
+// Получить комментарии для поста
+function getCommentsByPostId(postId) {
+  return comments.filter((comment) => comment.post_id === postId);
+}
+
+// Удалить комментарии для поста
+function deleteCommentsByPostId(postId) {
+  const initialLength = comments.length;
+  for (let i = comments.length - 1; i >= 0; i--) {
+    if (comments[i].post_id === postId) {
+      comments.splice(i, 1);
+    }
+  }
+  console.log(
+    `Удалено ${initialLength - comments.length} комментариев для поста с ID ${postId}.`
+  );
+}
+
+module.exports = {
+  getPostsByUser,
+  removePostById,
+  getAllPosts,
+  deleteAllPostsByUserId,
+  getCommentsByPostId,
+  deleteCommentsByPostId,
+};
--- задание 5/main.test.js
+++ задание 5/main.test.js
@@ -1,51 +1,102 @@
-// __tests__/main.test.js
-
-
-const { getPostsByUser } = require('./main'); 
+const {
+  getPostsByUser,
+  removePostById,
+  getAllPosts,
+  deleteAllPostsByUserId,
+  getCommentsByPostId,
+  deleteCommentsByPostId,
+} = require('./main');
 
 const user1 = {
   id: 1,
   name: "Иван",
   surname: "Иванов",
-  login: "ivanov123"
+  login: "ivanov123",
 };
 
 const user2 = {
   id: 2,
   name: "Петр",
   surname: "Петров",
-  login: "petrov456"
+  login: "petrov456",
 };
 
-describe('Тестирование функции getPostsByUser', () => {
-
-  test('Возвращает посты для пользователя Иван', () => {
-    const ivanPosts = getPostsByUser(user1.id);
-    expect(ivanPosts).toHaveLength(1); 
-    expect(ivanPosts[0].title).toBe('пост1'); 
+describe('Тестирование функций работы с постами и комментариями', () => {
+  beforeEach(() => {
+    jest.resetModules();
   });
 
-  test('Возвращает посты для пользователя Петр', () => {
-    const petrovPosts = getPostsByUser(user2.id);
-    expect(petrovPosts).toHaveLength(2); 
-    expect(petrovPosts[0].title).toBe('пост2'); 
-    expect(petrovPosts[1].title).toBe('пост3'); 
+  describe('Тестирование функции getPostsByUser', () => {
+    test('Проверка, что есть посты для Ивана', () => {
+      const ivanPosts = getPostsByUser(user1.id);
+      expect(ivanPosts).toHaveLength(1);
+      expect(ivanPosts[0].title).toBe('пост1');
+    });
+
+    test('Проверка, что есть посты у Петра', () => {
+      const petrovPosts = getPostsByUser(user2.id);
+      expect(petrovPosts).toHaveLength(2);
+      expect(petrovPosts[0].title).toBe('пост2');
+      expect(petrovPosts[1].title).toBe('пост3');
+    });
+
+    test('Возвращает пустой массив, если у пользователя нет постов', () => {
+      const nonExistentUser = { id: 3, name: "Алексей" };
+      const alexeyPosts = getPostsByUser(nonExistentUser.id);
+      expect(alexeyPosts).toHaveLength(0);
+    });
   });
 
-  test('Возвращает пустой массив, если у пользователя нет постов', () => {
-    const nonExistentUser = { id: 3, name: "Алексей" };
-    const alexeyPosts = getPostsByUser(nonExistentUser.id);
-    expect(alexeyPosts).toHaveLength(0); 
+  describe('Тестирование функции removePostById', () => {
+    test('Ничего не удаляет, если ID не найден', () => {
+      const initialPosts = getAllPosts();
+      removePostById(99);
+      const remainingPosts = getAllPosts();
+      expect(remainingPosts).toEqual(initialPosts);
+    });
   });
 
-  test('Проверка на правильность фильтрации по user_id', () => {
-    const ivanPosts = getPostsByUser(user1.id);
-    expect(ivanPosts.every(post => post.user_id === user1.id)).toBe(true);
+  describe('Тестирование функции getAllPosts', () => {
+    test('Возвращает все посты', () => {
+      const allPosts = getAllPosts();
+      expect(allPosts).toHaveLength(3);
+    });
   });
 
-  test('Проверка на отсутствие постов для другого пользователя', () => {
-    const petrovPosts = getPostsByUser(user2.id);
-    expect(petrovPosts.some(post => post.user_id === user1.id)).toBe(false);
+  describe('Тестирование функции deleteAllPostsByUserId', () => {
+    test('Удаляет все посты пользователя', () => {
+      deleteAllPostsByUserId(user2.id);
+      const remainingPosts = getAllPosts();
+      expect(remainingPosts).toHaveLength(1);
+      expect(remainingPosts[0].user_id).toBe(user1.id);
+    });
   });
 
+  describe('Тестирование функции getCommentsByPostId', () => {
+    test('Возвращает комментарии для указанного поста', () => {
+      const comments = getCommentsByPostId(1);
+      expect(comments).toHaveLength(1);
+      expect(comments[0].text).toBe('Отличный пост!');
+    });
+
+    test('Возвращает пустой массив, если у поста нет комментариев', () => {
+      const comments = getCommentsByPostId(99); 
+      expect(comments).toHaveLength(0); 
+    });
+  });
+
+  describe('Тестирование функции deleteCommentsByPostId', () => {
+    test('Удаляет комментарии для указанного поста', () => {
+      deleteCommentsByPostId(1);
+      const remainingComments = getCommentsByPostId(1);
+      expect(remainingComments).toHaveLength(0); 
+    });
+
+    test('Ничего не удаляет, если у поста нет комментариев', () => {
+      const initialComments = getCommentsByPostId(99); 
+      deleteCommentsByPostId(99);
+      const remainingComments = getCommentsByPostId(99);
+      expect(remainingComments).toEqual(initialComments); 
+    });
+  });
 });
